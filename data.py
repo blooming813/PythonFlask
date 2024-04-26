6acc4e1f53349889bdb441a36885c1c9bf2517b5
blooming813 <blooming.anna0813@gmail.com>
2024-04-26 00:56:46 -0400

    register step

---
+++ data.py
@@ -0,0 +1,8 @@
+def Articles():
+    articles = [
+        {'id': 1, 'title': 'Article one', 'body': 'lorem', 'author': 'Brad Traversy', 'create_date': '04-25-2017'},
+        {'id': 2, 'title': 'Article two', 'body': 'lorem', 'author': 'Charlie Traversy', 'create_date': '05-25-2019'},
+        {'id': 3, 'title': 'Article three', 'body': 'lorem', 'author': 'Hamilton Traversy', 'create_date': '01-25-2020'},
+        {'id': 4, 'title': 'Article four', 'body': 'lorem', 'author': 'John Traversy', 'create_date': '08-24-2015'}
+    ]
+    return articles
